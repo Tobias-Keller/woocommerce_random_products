0e4d968f3646574e2186c4cefb8aee46887fd9b7
p.tobias@hotmail.de <P.Tobias@hotmail.de>
2020-09-06 12:37:18 +0200

    reset some settings

--- woocommerce_random_products.php
+++ woocommerce_random_products.php
@@ -4,18 +4,18 @@ Plugin Name: Woocommerce random products
 Plugin URI: https://plugins.tobier.de
 Author URI: https://tobier.de
 Description: create random products for woocommerce
-Version: 0.1.6
-Icon1x: http://plugins.tobier.de/wp-content/uploads/2020/09/icon-128x128-1.png
-Icon2x: http://plugins.tobier.de/wp-content/uploads/2020/09/icon-256x256-1.png
-BannerHigh: http://plugins.tobier.de/wp-content/uploads/2020/09/banner-1544x500-1.png
-BannerLow: http://plugins.tobier.de/wp-content/uploads/2020/09/banner-772x250-1.png
+Version: 0.1.1
+Icon1x: https://plugins.tobier.de/wp-content/uploads/2020/09/icon-128x128-1.png
+Icon2x: https://plugins.tobier.de/wp-content/uploads/2020/09/icon-256x256-1.png
+BannerHigh: https://plugins.tobier.de/wp-content/uploads/2020/09/banner-1544x500-1.png
+BannerLow: https://plugins.tobier.de/wp-content/uploads/2020/09/banner-772x250-1.png
 */
 defined( 'ABSPATH' ) || die();
 
 require_once plugin_dir_path( __FILE__ ) . 'lib/wp-package-updater/class-wp-package-updater.php';
 
 $tobier_updater = new WP_Package_Updater(
- 	'http://plugins.tobier.de',
+ 	'https://plugins.tobier.de',
  	wp_normalize_path( __FILE__ ),
 	wp_normalize_path( plugin_dir_path( __FILE__ ) )
 );
